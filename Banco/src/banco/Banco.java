7d4c60dcfdee2dd7a59a2d8b12c9251fcb30f877
Rafaelhss <rafaelhss@gmail.com>
2018-04-24 21:37:14 -0300

    Add files via upload

--- Banco/src/banco/Banco.java
+++ Banco/src/banco/Banco.java
@@ -33,11 +33,34 @@ public class Banco {
         
         Transacao tx1 = new Transacao();
         tx1.tipo = 1; // DEBITO
-        tx1.valor = 2000;
+        tx1.valor = corre1.conta1.saldo;
+        corre1.conta1.aplicarTransacao(tx1);
         
-        corre1.conta2.aplicarTransacao(tx1);
+        Transacao tx2 = new Transacao();
+        tx2.tipo = 0; //CREDITO
+        tx2.valor = tx1.valor;
+        corre2.conta2.aplicarTransacao(tx2);
+        
+        
+        Transacao tx3 = new Transacao();
+        tx3.tipo = 1; //DEBITO
+        tx3.valor = corre2.conta1.saldo;
+        corre2.conta1.aplicarTransacao(tx3);
+        
+        Transacao tx4 = new Transacao();
+        tx4.tipo = 0;
+        tx4.valor = tx3.valor;
+        corre1.conta2.aplicarTransacao(tx4);
        
+        System.out.println("Saldo 1 da Maria:" + corre1.conta1.saldo);
         System.out.println("Saldo 2 da Maria:" + corre1.conta2.saldo);
+        System.out.println("Saldo total da Maria:" + corre1.saldoTotal());
+        
+        System.out.println("");
+        
+        System.out.println("Saldo 1 da Rosa:" + corre2.conta1.saldo);
+        System.out.println("Saldo 2 da Rosa:" + corre2.conta2.saldo);
+        System.out.println("Saldo total da Rosa:" + corre2.saldoTotal());
         
     }
     
